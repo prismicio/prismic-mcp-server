d8f1bf2f8d63adda7ab3de077fbfb892f69a9eb1
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-09-02 15:09:55 +0100

    feat: read slices and custom types

--- src/tools/generate_types.ts
+++ src/tools/generate_types.ts
@@ -1,7 +1,14 @@
+import { join as joinPath } from "path"
 import { z } from "zod"
 
 import { formatErrorForMcpTool } from "../lib/error"
 import { tool } from "../lib/mcp"
+import {
+	CustomType,
+	SharedSlice,
+} from "@prismicio/types-internal/lib/customtypes"
+
+import { readFile, readdir } from "fs/promises"
 
 export const generate_types = tool(
 	"generate_types",
@@ -13,17 +20,22 @@ RETURNS: TBD
 	z.object({
 		projectRootPath: z.string().describe("Absolute path to the project root"),
 	}).shape,
-	(args) => {
-		const { projectRootPath: _ } = args
+	async (args) => {
+		const { projectRootPath } = args
 
 		try {
-			// TODO: Implement
+			const customTypeModels = await readAllCustomTypeModels({
+				projectRootPath,
+			})
+			const sliceModels = await readAllSliceModels({
+				projectRootPath,
+			})
 
 			return {
 				content: [
 					{
 						type: "text",
-						text: "To be implemented",
+						text: JSON.stringify({ customTypeModels, sliceModels }, null, 2),
 					},
 				],
 			}
@@ -32,3 +44,71 @@ RETURNS: TBD
 		}
 	},
 )
+
+async function readAllCustomTypeModels(args: { projectRootPath: string }) {
+	const { projectRootPath } = args
+
+	const libraryDir = joinPath(projectRootPath, "customtypes")
+	const childDirs = await readdir(libraryDir, { withFileTypes: true })
+
+	return Promise.all(
+		childDirs.map(async (childDir) => {
+			const modelPath = joinPath(libraryDir, childDir.name, "index.json")
+			const modelContents = JSON.parse(await readFile(modelPath, "utf8"))
+
+			const parsedModel = CustomType.decode(modelContents)
+			if (parsedModel._tag === "Left") {
+				throw new Error(
+					`Invalid custom type model. ${parsedModel.left.join(", ")}`,
+				)
+			}
+
+			return parsedModel.right
+		}),
+	)
+}
+
+async function readAllSliceModels(args: { projectRootPath: string }) {
+	const { projectRootPath } = args
+
+	const slices: SharedSlice[] = []
+	const libraryPaths = await getSliceLibraryPaths(projectRootPath)
+
+	await Promise.all(
+		libraryPaths.map(async (libraryPath) => {
+			const libraryDir = joinPath(projectRootPath, libraryPath)
+			const childDirs = await readdir(libraryDir, { withFileTypes: true })
+
+			await Promise.all(
+				childDirs.map(async (childDir) => {
+					if (!childDir.isDirectory()) {
+						return
+					}
+
+					const modelPath = joinPath(libraryDir, childDir.name, "model.json")
+					const modelContents = JSON.parse(await readFile(modelPath, "utf8"))
+
+					const parsedModel = SharedSlice.decode(modelContents)
+					if (parsedModel._tag === "Left") {
+						throw new Error(
+							`Invalid slice model. ${parsedModel.left.join(", ")}`,
+						)
+					}
+
+					slices.push(parsedModel.right)
+				}),
+			)
+		}),
+	)
+
+	return slices
+}
+
+const SliceMachineConfig = z.object({ libraries: z.array(z.string()) })
+
+async function getSliceLibraryPaths(projectRoot: string) {
+	const configFilePath = joinPath(projectRoot, "slicemachine.config.json")
+	const config = await readFile(configFilePath, "utf8")
+
+	return SliceMachineConfig.parse(JSON.parse(config)).libraries
+}
