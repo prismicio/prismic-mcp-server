23f192ff687f115cdac44f76a6ed509f14c4f037
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-09-04 15:36:43 +0100

    [DT-2875] add generate_types tool (#39)

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { Telemetry } from "./lib/telemetry"
 
 import { name, version } from "../package.json"
 
+import { generate_types } from "./tools/generate_types"
 import { how_to_code_slice } from "./tools/how_to_code_slice"
 import { how_to_upsert_mock_slice } from "./tools/how_to_mock_slice"
 import { how_to_model_slice } from "./tools/how_to_model_slice"
@@ -17,3 +18,4 @@ server.tool(...how_to_code_slice)
 server.tool(...how_to_model_slice)
 server.tool(...verify_slice_model)
 server.tool(...how_to_upsert_mock_slice)
+server.tool(...generate_types)
