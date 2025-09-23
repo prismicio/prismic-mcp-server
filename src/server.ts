0ff17fa1561a507750cdeb9f19a6aec228142353
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-09-23 10:23:19 +0100

    refactor: remove generate_types and verify_slice_code tools
    chore(deps): install SM manager

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,9 @@ import { Telemetry } from "./lib/telemetry"
 import { name, version } from "../package.json"
 
 import { add_slice_to_custom_type } from "./tools/add_slice_to_custom_type"
-import { generate_types } from "./tools/generate_types"
 import { how_to_code_slice } from "./tools/how_to_code_slice"
 import { how_to_mock_slice } from "./tools/how_to_mock_slice"
 import { how_to_model_slice } from "./tools/how_to_model_slice"
-import { verify_slice_code } from "./tools/verify_slice_code"
 import { verify_slice_mock } from "./tools/verify_slice_mock"
 import { verify_slice_model } from "./tools/verify_slice_model"
 
@@ -22,6 +20,4 @@ server.tool(...how_to_model_slice)
 server.tool(...verify_slice_model)
 server.tool(...how_to_mock_slice)
 server.tool(...verify_slice_mock)
-server.tool(...generate_types)
 server.tool(...add_slice_to_custom_type)
-server.tool(...verify_slice_code)
