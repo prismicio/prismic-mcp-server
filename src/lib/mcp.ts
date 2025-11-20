49e8fc5034cdf28f1277c2eff73e2be20b7041e6
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-11-20 15:43:32 +0000

    chore: cleanup

--- src/lib/mcp.ts
+++ src/lib/mcp.ts
@@ -1,6 +1,5 @@
 import type {
 	McpServer,
-	PromptCallback,
 	ToolCallback,
 } from "@modelcontextprotocol/sdk/server/mcp.js"
 import type { ZodRawShape } from "zod"
@@ -11,22 +10,20 @@ export const resource = <T extends Parameters<McpServer["resource"]>>(
 	...args: T
 ): T => args
 
-type ToolArgs<Args extends ZodRawShape> = [
+export const tool = <Args extends ZodRawShape>(
+	...args: [
+		name: string,
+		description: string,
+		paramsSchemaOrAnnotations: Args | ToolAnnotations,
+		cb: ToolCallback<Args>,
+	]
+): [
 	name: string,
 	description: string,
 	paramsSchemaOrAnnotations: Args | ToolAnnotations,
 	cb: ToolCallback<Args>,
-]
-export const tool = <Args extends ZodRawShape>(
-	...args: ToolArgs<Args>
-): ToolArgs<Args> => args
+] => args
 
-type PromptArgs<Args extends ZodRawShape> = [
-	name: string,
-	description: string,
-	argsSchema: Args,
-	cb: PromptCallback<Args>,
-]
-export const prompt = <Args extends ZodRawShape>(
-	...args: PromptArgs<Args>
-): PromptArgs<Args> => args
+export const prompt = <T extends Parameters<McpServer["prompt"]>>(
+	...args: T
+): T => args
